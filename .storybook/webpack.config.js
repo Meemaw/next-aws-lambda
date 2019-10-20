b8a7e2c2717127a5b7c244c976f44df51f9d44e0
Matej Snuderl <matej.snuderl@cognite.com>
2019-10-20 10:25:21 +0200

    chore: support absolute imports inside storybooks

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
@@ -10,6 +12,11 @@ module.exports = ({ config }) => {
       },
     ],
   });
+
+  // Required for absolute imports in Storybook
+  const resolvedModules = config.resolve.modules || [];
+  config.resolve.modules = [...resolvedModules, path.resolve(process.cwd(), 'src')];
+
   config.resolve.extensions.push('.ts', '.tsx');
   return config;
 };
---
+++ src/containers/AboutPage/AboutPage.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import AboutPage from './AboutPage';
+
+export default {
+  title: 'AboutPage',
+};
+
+export const Default = () => <AboutPage />;
