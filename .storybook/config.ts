ec97e85548b832052c5fe5a1b78239dc5cdffe8d
Matej Šnuderl <matej.snuderl@cognite.com>
2020-01-23 18:11:46 +0100

    chore: bump dependencies (#9)

--- .storybook/config.ts
+++
@@ -1,8 +0,0 @@
-import { configure } from '@storybook/react';
-
-function loadStories() {
-  const req = require.context('../src', true, /\.stories\.tsx$/);
-  return req.keys().map(req);
-}
-
-configure(loadStories, module);
